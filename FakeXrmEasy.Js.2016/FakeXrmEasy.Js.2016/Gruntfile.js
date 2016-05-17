323cd1844d8a29a67301c9fc4f403d9223211956
admin <jordi.montana@gmail.com>
2016-05-18 01:43:24 +0200

    Adding istanbul code coverage metrics

--- FakeXrmEasy.Js.2016/FakeXrmEasy.Js.2016/Gruntfile.js
+++ FakeXrmEasy.Js.2016/FakeXrmEasy.Js.2016/Gruntfile.js
@@ -1,46 +1,61 @@
 ﻿// in Gruntfile.js
 module.exports = function (grunt) {
 
+    'use strict';
+
+    var dateFormat = require('dateformat');
+
+    //var tests = 'test/**/*_test.js';
+    //var tasks = 'tasks/**/*.js';
+    //var reportDir = 'build/reports/' + dateFormat(new Date(), 'yyyymmdd-HHMMss');
+
     grunt.initConfig({
         env: {
             coverage: {
-                APP_DIR_FOR_CODE_COVERAGE: '../test/coverage/instrument/app/'
+                APP_DIR_FOR_CODE_COVERAGE: 'tests/coverage/instrument/'
             }
         },
         instrument: {
             files: 'src/*.js',
             options: {
                 lazy: true,
-                basePath: 'test/coverage/instrument/'
+                basePath: 'tests/coverage/instrument/'
             }
         },
         mochaTest: {
             options: {
                 reporter: 'spec'
             },
-            src: ['tests/*.js']
+            src: ['tests/coverage/instrument/src/*.js', 'tests/*.js']
         },
         storeCoverage: {
             options: {
-                dir: 'test/coverage/reports'
+                dir: 'tests/coverage/reports'
             }
         },
+        reloadTasks: {
+            rootPath: 'build/instrument/tasks'
+        },
         makeReport: {
-            src: 'test/coverage/reports/**/*.json',
+            src: 'tests/coverage/reports/**/*.json',
             options: {
                 type: 'lcov',
-                dir: 'test/coverage/reports',
+                dir: 'tests/coverage/reports',
                 print: 'detail'
             }
         }
     });
 
+    grunt.loadNpmTasks('grunt-env');
+    grunt.loadNpmTasks('grunt-mocha-test');
+    grunt.loadNpmTasks('grunt-istanbul');
 
+    /*grunt.registerTask('coverage', ['env:coverage', 'instrument', 'mochaTest',
+        'storeCoverage', 'makeReport']); */
+
+    grunt.registerTask('coverage', [ 'instrument', 'mochaTest',
+        'storeCoverage', 'makeReport']);
 };
 
-grunt.loadNpmTasks('grunt-mocha-test');
-grunt.loadNpmTasks('grunt-istanbul');
 
-grunt.registerTask('coverage', ['env:coverage', 'instrument', 'mochaTest',
-    'storeCoverage', 'makeReport']);
 
