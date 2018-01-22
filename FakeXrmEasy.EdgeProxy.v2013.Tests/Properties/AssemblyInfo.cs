7100728c23e294535a89b39292a379b2e16498a0
admin <jordi.montana@gmail.com>
2018-01-23 00:48:02 +0100

    Added 2013 unit test project

---
+++ FakeXrmEasy.EdgeProxy.v2013.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("FakeXrmEasy.EdgeProxy.v2013.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FakeXrmEasy.EdgeProxy.v2013.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("af99fc0b-6738-4301-9c6b-9448fce50d9d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
