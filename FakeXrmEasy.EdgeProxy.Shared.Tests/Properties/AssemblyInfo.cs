e85e403e7f98045115da767960072773b957859e
admin <jordi.montana@gmail.com>
2018-01-23 00:08:38 +0100

    Added Remaining EdgeProxy projects, one for each major version

---
+++ FakeXrmEasy.EdgeProxy.Shared.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("FakeXrmEasy.EdgeProxy.Shared.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FakeXrmEasy.EdgeProxy.Shared.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("000a5e98-3a1e-4972-bace-f8a3d21c7dd8")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
