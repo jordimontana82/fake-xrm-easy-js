1d26b6f016062b1495ef0b8557c1983152efcd92
admin <jordi.montana@gmail.com>
2018-01-22 21:54:11 +0100

    Moved EdgeProxy to a Shared project to build multiple nuget packages with the appropiate fakexrmeasy version

---
+++ FakeXrmEasy.EdgeProxy.Shared/Extensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EdgeProxy
+{
+    public class Extensions
+    {
+        public static bool DynamicContainsProperty(dynamic dyn, string name)
+        {
+            Type objType = dyn.GetType();
+
+            if (objType == typeof(ExpandoObject))
+            {
+                return ((IDictionary<string, object>)dyn).ContainsKey(name);
+            }
+
+            return dyn.GetProperty(name) != null;
+        }
+
+    }
+}
