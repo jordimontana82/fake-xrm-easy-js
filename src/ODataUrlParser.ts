306505ded6a310c1d2e2c2828a77ba3f95c94e7e
Jordi <jordi.montana@gmail.com>
2019-03-26 01:35:39 +0100

    fix regular expression check in odata url parser was broke queries tests

--- src/ODataUrlParser.ts
+++ src/ODataUrlParser.ts
@@ -20,7 +20,7 @@ export default class ODataUrlParser implements IODataUrlParser {
         var entityNameWithIdRegex = /^([a-z0-9_])*\([0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}\)$/i;
         var match = entityNameWithIdRegex.exec(result.entitySetName);
 
-        if(match.length > 0) {
+        if(match && match.length > 0) {
             var split = result.entitySetName.split('(');
             result.entitySetName = split[0];
             result.id = split[1].replace(")", "");
